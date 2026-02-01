eec597ee7be6bcde4916a217b10ac627e776e2b7
Marco Bacci <marcobacci94@gmail.com>
2026-02-01 12:59:40 +0100

    Fix: change name in ThemeProvider

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export function GlobalProvider({ children }) {
+export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
