4ae2aa9b5de53ad45d5adc6ad7644e00763133e5
Marco Bacci <marcobacci94@gmail.com>
2026-02-01 12:51:58 +0100

    Feat: create context to change theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 import App from "./App.jsx";
+import { GlobalProvider } from "./contexts/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
